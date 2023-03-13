00780ad3c8f966b7be9e6a700ac00a3399f98a00
claud_park <claudia@relate.so>
2023-03-13 10:07:06 +0900

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '../styles/globals.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+function MyApp({ Component, pageProps }) {
+  const client = new ApolloClient({
+    uri: 'http://backendonline.codebootcamp.co.kr/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
